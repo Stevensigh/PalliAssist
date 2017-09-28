21667f726b60dcb5482aa47fb57fb652101ec5d7
stevensigh <steven.tsai95@gmail.com>
2017-09-27 21:18:48 -0500

    update?

---
+++ src/app/app.constants.ts
@@ -0,0 +1,20 @@
+export const firebaseConfig = {
+    apiKey: "<APIKEY>",
+    authDomain: "<AUTHDOMAIN>",
+    databaseURL: "<DATABASEURL>",
+    projectId: "<PROJECTID>",
+    storageBucket: "<STORAGEBUCKET>",
+    messagingSenderId: "<SENDERID>"
+}
+
+/** google webClientId */
+export const googleWebClientId = 'GOOGLE WEB CLIENT ID';
+
+/** firebase tables */
+export const tableNames = {
+    User               : 'users',
+    Contacts           : 'contacts',
+    ChatChannel        : 'chat_channels',
+    ChatMember         : 'chat_members',
+    ChatMessage        : 'chat_messages',
+};
